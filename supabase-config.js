ada44d8dc27a1f23fcfc3a1c9236bf3aad33ee65
hhj893387-wq <hhj893387@gmail.com>
2026-07-24 19:18:02 +0500

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://zhpxojimjyfheccdwucd.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpocHhvamltanlmaGVjY2R3dWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4Nzc3OTQsImV4cCI6MjEwMDQ1Mzc5NH0.zsgB8YRnQnyEc4Tngpn5rQFqiphX3B6juQ_pwik1J1s";
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
